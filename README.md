abec75ef363a027698770bf5a67fa145ed651d83
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-05-07 20:12:51 +0200

    add ts

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>Webpack setup completed</h1>
+
+:hammer_and_wrench: Languages and Tools :
+<div>
+  <img src="https://github.com/devicons/devicon/blob/master/icons/sass/sass-original.svg" title="Sass" alt="Sass" width="40" height="40"/>&nbsp;
+  <img src="https://github.com/devicons/devicon/blob/master/icons/javascript/javascript-original.svg" title="JS" alt="JS" width="40" height="40"/>&nbsp;
+  <img src="https://github.com/devicons/devicon/blob/master/icons/npm/npm-original-wordmark.svg" title="npm" alt="npm" width="40" height="40"/>&nbsp;
+   <img src="https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/webpack/webpack-original-wordmark.svg" title="WebPack" alt="WebPack" width="40" height="40"/>&nbsp;
+<img src="https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/typescript/typescript-original.svg" title="TypeScript" alt="TypeScript" width="40" height="40"/>&nbsp;
+
+</div>
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,50 @@
 import './styles.scss';
+
+function slider(){
+    const prevButton : HTMLElement | null = document.querySelector('.prevButton');
+    const nextButton : HTMLElement | null = document.querySelector('.nextButton');
+
+    prevButton.addEventListener('click', ()=>{
+        console.log('1');
+    })
+}
+slider();
+
+// Определение типа для NodeList, который возвращает querySelectorAll
+interface NodeList<T> extends NodeListOf<any> {}
+
+// Определение типа для элемента, который может быть получен через querySelector
+interface HTMLElementWithClass<T> extends HTMLElement {
+    classList: DOMTokenList & { [key: string]: boolean };
+}
+function increaseImg() {
+    try {
+        const licenseImages: NodeList<HTMLElementWithClass<HTMLImageElement>> = document.querySelectorAll('.license-img');
+        const enlargedImages: NodeList<HTMLElementWithClass<HTMLDivElement>> = document.querySelectorAll('.enlarged-image');
+        
+        // Проверяем, что коллекция enlargedImages не пуста
+        if (enlargedImages.length === 0) throw new Error("Enlarged image not found");
+        
+        // Преобразуем NodeList в массив для удобства работы
+        const enlargedImagesArray = Array.from(enlargedImages);
+
+        licenseImages.forEach((licenseImg, index) => {
+            const enlargedImage = enlargedImagesArray[index];
+           
+            licenseImg.addEventListener('click', () => {
+                enlargedImage.classList.add('active');
+            });
+        });
+
+        enlargedImagesArray.forEach(enlargedImage => {
+            enlargedImage.addEventListener('click', ()=>{
+                enlargedImage.classList.remove('active');
+                console.log('123');
+            })
+        })
+    } catch (error) {
+        console.error("Error in increaseImg function:", error);
+    }
+}
+
+increaseImg();
